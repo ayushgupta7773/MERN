8818e08504ed27fde309bd365168e655b5554883
Ayush Gupta <117255251+devilthekiller@users.noreply.github.com>
2024-01-18 17:03:15 +0530

    dd

--- advanced_React/use-effect/src/multiple-return-fetch.jsx
+++ advanced_React/use-effect/src/multiple-return-fetch.jsx
@@ -13,19 +13,29 @@ export const MultipleReturnFetch = () => {
         const user = await response.json();
 
         if (!response.ok) {
+          setIsError(true);
           throw new Error("Network response was not ok");
         }
-
+ 
         setUser(user); // Update the state with the fetched user data
-        // console.log(user); // Log the data on the initial render
+        console.log(user); // Log the data on the initial render
       } catch (error) {
+        setIsError(true)
         console.error("Error fetching data:", error);
       }
+      setIsLoading(false)
     };
 
     console.log(user);  
     fetchData();
   }, []);
+
+  if (isLoading){
+    return (<h2>Loading...</h2>)
+  }
+  if(isError){
+    return (<h2>An Error Occured</h2>);
+  }
 
   return (
     <div>  
