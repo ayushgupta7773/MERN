caf63c8d65d8202d0b6c9072f108c5d20ea66e02
Ayush Gupta <117255251+devilthekiller@users.noreply.github.com>
2024-02-01 16:24:10 +0530

    sk
    new

---
+++ NodeJSExpress/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+// app.use(cors(
+//     {
+//         origin:'*'
+//     }
+// ));
+
+app.listen(5000,()=>{
+    
+})
+
+const mongoose = require('mongoose');
+mongoose.connect("")
--- advanced_React/use-effect/src/multiple-return-fetch.jsx
+++ advanced_React/use-effect/src/multiple-return-fetch.jsx
@@ -1,15 +1,17 @@
 import { useEffect, useState } from "react";
 
 export const MultipleReturnFetch = () => {
-  const [isLoading,  setIsLoading] = useState(true);
-  const [isError,setIsError]=useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
 
   const [user, setUser] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('https://api.github.com/users/devilthekiller');
+        const response = await fetch(
+          "https://api.github.com/users/devilthekiller"
+        );
         const user = await response.json();
 
         if (!response.ok) {
@@ -20,29 +22,29 @@ export const MultipleReturnFetch = () => {
         setUser(user); // Update the state with the fetched user data
         console.log(user); // Log the data on the initial render
       } catch (error) {
-        setIsError(true)
+        setIsError(true);
         console.error("Error fetching data:", error);
       }
-      setIsLoading(false)
+      setIsLoading(false);
     };
 
-    console.log(user);  
+    console.log(user);
     fetchData();
   }, []);
 
-  if (isLoading){
-    return (<h2>Loading...</h2>)
+  if (isLoading) {
+    return <h2>Loading...</h2>;
   }
-  if(isError){
-    return (<h2>An Error Occured</h2>);
+  if (isError) {
+    return <h2>An Error Occured</h2>;
   }
 
   return (
-    <div>  
+    <div>
       {user && (
         <div key={user.id}>
           {/* You can render any component-specific JSX here if needed */}
-          <img src={user.avatar_url} alt=""/>
+          <img src={user.avatar_url} alt="" />
           <h2>{user.name}</h2>
           <p>{user.bio}</p>
         </div>
